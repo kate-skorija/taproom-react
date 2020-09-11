6618d19876cb82dac803ef7c5b4e479458f40311
Kate Skorija <kateskorija@gmail.com>
2020-09-11 09:21:01 -0700

    create toggling button for keg list/new keg build out new keg form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { v4 } from 'uuid';
 
 function NewKegForm(props){
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.brand.value);
+    console.log(event.target.price.value);
+  }
+
   return (
     <React.Fragment>
-      <h3>Test form.</h3>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input 
+        type = 'text'
+        name = 'name'
+        placeHolder = 'Name of Beer'
+        />
+        <input 
+        type = 'text'
+        brand = 'brand'
+        placeHolder = 'Brand'
+        />
+        <input 
+        type = 'text'
+        price = 'price'
+        placeHolder = 'Price per Pint'
+        />
+        <input 
+        type = 'text'
+        alcoholContent = 'alcoholContent'
+        placeHolder = 'ABV%'
+        />
+        {/* <input 
+        type = 'number'
+        name = 'pintsRemaining'
+        placeHolder = '124'
+        /> */}
+      </form>
     </React.Fragment>
   );
 }
